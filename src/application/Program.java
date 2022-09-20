ea594ee10edf40c0f86a553b4bd570e953f9b117
Marcelo Moura <mgmoura@gmail.com>
2022-09-20 14:54:58 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,24 @@
 package application;
 
-import java.util.Date;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();  
+		
+		System.out.println("= = TEST # 01: seller findById = =");
+		System.out.print("Digite o id a ser pesquisado: ");
+		int pesquisa = scan.nextInt();
 
-		Seller seller = sellerDao.findById(1);
+		Seller seller = sellerDao.findById(pesquisa);
 		
 		System.out.println(seller);
 
